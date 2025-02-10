ef2917b651ae0878881b8df532a8fe08bda29098
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-10 08:48:13 +0100

    added login with matched password from hashed password and user gets a token for one hour in auth.controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,7 @@ const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
+//register a new user
 exports.register = async (request, h) => {
     const { username, password } = request.payload;
 
@@ -13,9 +14,33 @@ exports.register = async (request, h) => {
 
         if (existingUser) {
             return h.response({message: 'Användarnamnet finns redan'}).code(400);
-        }else {
+        } else {
             const newUser = new User ({username, password});
             await newUser.save();
+        }
+        
+    } catch (error) {
+        return h.response({ message: error.message }).code(500);
+    }
+};
+
+//login
+exports.login = async (request, h) => {
+    const { username, password } = request.payload;
+
+    try {
+
+        const user = await User.findOne({username});
+        const matchedPassword= await user.comparePassword(password);
+
+        if (!user|| !matchedPassword) {
+            return h.response({message: 'Felaktigt användarnamn eller lösenord'}).code(401);
+        } else {
+            const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, {
+                expiresIn: '1h'// jwt token valid for one hour
+            });
+    
+            return h.response({ token }).code(200);
         }
         
     } catch (error) {
